26db9cfff31216fec676e09ebbb36c158ab0f669
simple710 <112814424+simple0710@users.noreply.github.com>
2024-01-06 18:17:56 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 144 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -4,34 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static String[][] defaultBoard;
+    static boolean[][] defaultBoard;
 
     public static int search(int x, int y) {
-        int whiteCnt = 0;
-        int blackCnt = 0;
+        int cnt = 0;
+        boolean color = defaultBoard[x][y];
         for (int i = x; i < x+8; i++) {
             for (int j = y; j < y+8; j++) {
-                if ((i + j) % 2 == 0) {
-                    if (defaultBoard[i][j].equals("B")) whiteCnt++;
-                    if (defaultBoard[i][j].equals("W")) blackCnt++;
-                } else {
-                    if (defaultBoard[i][j].equals("W")) whiteCnt++;
-                    if (defaultBoard[i][j].equals("B")) blackCnt++;
-                }
+                if (color != defaultBoard[i][j]) cnt++;
+                color = !color;
             }
+            color = !color;
         }
-        return Math.min(whiteCnt, blackCnt);
+        return Math.min(64-cnt, cnt);
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        defaultBoard = new String[N][M];
+        defaultBoard = new boolean[N][M];
         for (int i = 0; i < N; i++) {
-            defaultBoard[i] = br.readLine().split("");
+            String[] row = br.readLine().split("");
+            for (int j = 0; j < M; j++) {
+                if (row[j].equals("W")) defaultBoard[i][j] = true;
+                else defaultBoard[i][j] = false;
+            }
         }
-        int res = 1000000000;
+        int res = 64;
         for (int i = 0; i <= N-8; i++) {
             for (int j = 0; j <= M-8; j++) {
                 res = Math.min(res, search(i, j));
