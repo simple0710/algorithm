ea7ed8e76ca30cd35c10926b9b5a50fdf4ca6613
simple710 <112814424+simple0710@users.noreply.github.com>
2024-01-10 21:05:23 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb;
+    static boolean[] visited;
+    static int[] arr;
+    static int N, M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        visited = new boolean[N+1];
+        back(0, 1);
+        System.out.print(sb);
+    }
+    
+    public static void back(int depth, int num) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) sb.append(arr[i]).append(" ");
+            sb.append("\n");
+            return;
+        }
+        for (int i = num; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i;
+                back(depth+1, i+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
