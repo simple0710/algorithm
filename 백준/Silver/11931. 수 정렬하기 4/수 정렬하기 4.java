929fd3e586685d3b32e4db45fe4783008c3093a3
simple710 <112814424+simple0710@users.noreply.github.com>
2024-01-14 05:00:04 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1420 ms, Memory: 129260 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) list.add(Integer.parseInt(br.readLine()));
+        Collections.sort(list, Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        for (int value : list) sb.append(value).append("\n");
+        System.out.print(sb);
+    }
+}
