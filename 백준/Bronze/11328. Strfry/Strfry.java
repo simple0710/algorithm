535af16f5bc8d64c6d93757b1c699031cf3d3670
simple710 <112814424+simple0710@users.noreply.github.com>
2024-01-18 23:32:33 +0900

    [Bronze II] Title: Strfry, Time: 1876 ms, Memory: 94396 KB -BaekjoonHub

---
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String[] A = st.nextToken().split("");
+			String[] B = st.nextToken().split("");
+			Arrays.sort(A);
+			Arrays.sort(B);
+			System.out.println(Arrays.equals(A, B) ? "Possible" : "Impossible");
+		}
+	}
+}
