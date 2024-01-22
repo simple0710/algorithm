06ab96a99f793c671f8bfc3a47490885a9d78aca
simple710 <112814424+simple0710@users.noreply.github.com>
2024-01-22 22:31:12 +0900

    [Silver II] Title: 스택 수열, Time: 368 ms, Memory: 26228 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,29 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
-        int N = Integer.parseInt(br.readLine());
-        int[] resArr = new int[N];
-        for (int i = 0; i < N; i++) resArr[i] = Integer.parseInt(br.readLine());
-        int idx = 0;
-        for (int i = 1; i <= N+1; i++) {
-            while (!stack.isEmpty() && stack.peek() == resArr[idx]) {
-                stack.pop();
-                sb.append("-").append("\n");
-                idx++;
-            }
-            if (i <= N) {
-                stack.add(i);
-                sb.append("+").append("\n");
-            }
-        }
-        if (stack.isEmpty()) System.out.print(sb);
-        else System.out.println("NO");
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		int[] resArr = new int[N];
+		for (int i = 0; i < N; i++) {
+			resArr[i] = Integer.parseInt(br.readLine());
+		}
+		StringBuilder sb = new StringBuilder();
+		int idx = 0;
+		for (int i = 1; i <= N; i++) {
+			stack.add(i);
+			sb.append("+\n");
+			while (!stack.isEmpty() && stack.peek() == resArr[idx]) {
+				sb.append("-\n");
+				stack.pop();
+				idx++;
+			}
+		}
+		System.out.print(idx == N ? sb : "NO");
+	}
 }
