3559062d710d1f0996f4c54d78b1c0218e16953f
simple710 <112814424+simple0710@users.noreply.github.com>
2024-01-31 00:07:32 +0900

    [Silver III] Title: N과 M (1), Time: 268 ms, Memory: 22404 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[] s;
-    static boolean[] visited;
-
-    static StringBuilder sb;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        sb = new StringBuilder();
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        visited = new boolean[N+1];
-        s = new int[M];
-        back(0);
-        System.out.print(sb);
-    }
-
-    public static void back(int depth) {
-        if (depth == M) {
-            for (int i = 0; i < M; i ++) sb.append(s[i]).append(" ");
-            sb.append("\n");
-            return;
-        }
-        for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                s[depth] = i;
-                back(depth+1);
-                visited[i] = false;
-            }
-        }
-    }
+	static int N, M;
+	static int[] s;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		visited = new boolean[N];
+		s = new int[M];
+		back(0);
+		System.out.print(sb);
+	}
+	public static void back(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < depth; i++) {
+				sb.append(s[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			if (visited[i]) continue;
+			visited[i] = true;
+			s[depth] = i+1;
+			back(depth+1);
+			visited[i] = false;
+		}
+	}
 }
