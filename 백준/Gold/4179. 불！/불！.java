71bcf683d31f690073f86c769ddd1a079930a45c
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-01 22:58:51 +0900

    [Gold IV] Title: 불!, Time: 544 ms, Memory: 66956 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -5,14 +5,18 @@ public class Main {
 	static class Place {
 		int x;
 		int y;
+
 		Place(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
 	}
+	
+	static boolean resFlag;
 	static int R, C;
 	static int[][] visited;
 	static char[][] board;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -35,27 +39,36 @@ public class Main {
 				}
 			}
 		}
-		bfs(fq, false);
-		System.out.print(bfs(q, true));
+		
+		System.out.print(bfs(q, fq));
+	}
+
+	public static String bfs(Deque<Place> q, Deque<Place> fq) {
+		int turn = 1;
+		while (!q.isEmpty()) {
+            fq = search(fq, false);
+			q = search(q, true);
+			if (resFlag) return String.valueOf(turn);
+			turn++;
+		}
+		return "IMPOSSIBLE";
 	}
 	
-	public static String bfs(Deque<Place> q, boolean flag) {
-		int[] dx = new int[] {-1, 1, 0, 0}, dy = new int[] {0, 0, -1 ,1};
-		if (flag) visited[q.getFirst().x][q.getFirst().y] = 1;
+	public static Deque<Place> search(Deque<Place> q, boolean flag) {
+		int[] dx = new int[] { -1, 1, 0, 0 }, dy = new int[] { 0, 0, -1, 1 };
+		Deque<Place> newQ = new LinkedList<>();
 		while (!q.isEmpty()) {
 			Place now = q.poll();
-			if (flag && (now.x == 0 || now.x == R-1 || now.y == 0 || now.y == C-1)) return String.valueOf(visited[now.x][now.y]);
+			if (flag && (now.x == 0 || now.x == R - 1 || now.y == 0 || now.y == C - 1)) resFlag = true;
 			for (int i = 0; i < 4; i++) {
 				int nx = now.x + dx[i];
 				int ny = now.y + dy[i];
-				if (0 <= nx && nx < R && 0 <= ny && ny < C && board[nx][ny] != '#') {
-					if (visited[nx][ny] == 0 || visited[nx][ny] > visited[now.x][now.y] + 1) {
-						visited[nx][ny] = visited[now.x][now.y] + 1;
-						q.add(new Place(nx, ny));
-					}
+				if (0 <= nx && nx < R && 0 <= ny && ny < C && board[nx][ny] != '#' && visited[nx][ny] == 0) {
+					visited[nx][ny] = visited[now.x][now.y] + 1;
+					newQ.add(new Place(nx, ny));
 				}
 			}
 		}
-		return "IMPOSSIBLE";
+		return newQ;
 	}
 }
