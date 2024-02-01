45822734c511672056353f2c4c12a206a8a2caf6
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-02 00:08:09 +0900

    [Gold V] Title: 적록색약, Time: 160 ms, Memory: 16492 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -3,36 +3,42 @@ import java.io.*;
 public class Main {
 	static int N;
 	static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
-	static char[][] board;
+	static char[][] rgbBoard, rbBoard;
 	static boolean[][][] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		board = new char[N][];
-		for (int i = 0; i < N; i++) board[i] = br.readLine().toCharArray();
-		int res1 = 0;
-		int res2 = 0;
+		rgbBoard = new char[N][N];
+		rbBoard = new char[N][N];
+		for (int i = 0; i < N; i++) {
+			char[] charArr = br.readLine().toCharArray();
+			for (int j = 0; j < N; j++) {
+				rgbBoard[i][j] = charArr[j];
+				rbBoard[i][j] = charArr[j] == 'B' ? 'B' : 'R';
+			}
+		}
+		int[] resArr = new int[2];
 		visited = new boolean[2][N][N];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (!visited[0][i][j]) {
-					dfs(board[i][j], 0, i, j);
-					res1++;
-				}
-				if (!visited[1][i][j]) {
-					dfs(board[i][j], 1, i, j);
-					res2++;
+				for (int k = 0; k < 2; k++) {
+					if (!visited[k][i][j]) {
+						dfs(k == 0 ? rgbBoard : rbBoard, k, i, j);
+						resArr[k]++;
+					}
 				}
 			}
 		}
-		System.out.print(res1 + " " + res2);
+		System.out.print(resArr[0] + " " + resArr[1]);
 	}
-	public static void dfs(char color, int w, int x, int y) {
-		if (x < 0 || x >= N || y < 0 || y >= N || visited[w][x][y]) return;
-		else if (w == 0 && color != board[x][y]) return;
-		else if (board[x][y] == 'B' && color !=  'B') return;
-		else if (color == 'B' && board[x][y] != 'B') return;
-		visited[w][x][y] = true;
-		for (int i = 0; i < 4; i++) dfs(color, w, x + dx[i], y + dy[i]);
+	public static void dfs(char[][] board, int w, int x, int y) {
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (0 <= nx && nx < N && 0 <= ny && ny < N && !visited[w][nx][ny] && board[x][y] == board[nx][ny]) {
+				visited[w][nx][ny] = true;
+				dfs(board, w, nx, ny);
+			}
+		}
 	}
 }
