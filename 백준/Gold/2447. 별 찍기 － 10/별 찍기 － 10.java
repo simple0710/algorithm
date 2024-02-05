b778d6814c2cae71ea9c76ac1e137817950663fc
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-05 20:26:55 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 400 ms, Memory: 57668 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,36 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
 
 public class Main {
-    static int[] dx = {-1, -1, -1, 0, 1, 1, 1, 0}, dy = {-1, 0, 1, 1, 1, 0, -1, -1};
-    static String[][] board;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        board = new String[N][N];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(board[i], "*");
-        }
-        recursion(N / 3, N / 2, N / 2);
-        for (int i = 0; i < N; i++) {
-            System.out.println(String.join("", board[i]));
-        }
-    }
-
-    public static void recursion(int n, int x, int y) {
-        if (n == 1) {
-            board[x][y] = " ";
-            return;
-        }
-        for (int i = x - n/2; i <= x+n/2; i++) {
-            for (int j = y - n/2; j <= y+n/2; j++) {
-                board[i][j] = " ";
-            }
-        }
-        for (int i = 0; i < 8; i++) {
-            recursion(n/3, x+n*dx[i], y+n*dy[i]);
-        }
-    }
+	static int N;
+	static int[] dx = new int[]{-1, -1, -1, 0, 0, 1, 1, 1}, dy = new int[]{-1, 0, 1, -1, 1, -1, 0, 1}; 
+	static String[][] board;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		board = new String[N][N];
+		recur(N/3, N/2, N/2);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(board[i][j] == null ? " " : "*");
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+	
+	public static void recur(int nowN, int centerX, int centerY) {
+		if (nowN == 1) {
+			for (int i = 0 ; i < 8; i++) board[centerX + dx[i]][centerY + dy[i]] = "*";
+			return;
+		}
+		for (int i = 0 ; i < 8; i++) recur(nowN/3, centerX + nowN * dx[i], centerY + nowN * dy[i]);
+	}
 }
