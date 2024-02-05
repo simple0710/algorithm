c86ad5084681a4104be2fdab95a82df05fdd291c
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-05 22:04:41 +0900

    [Silver II] Title: 유기농 배추, Time: 168 ms, Memory: 15764 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,38 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Place {
-		int x;
-		int y;
-		Place(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-	static int N, M, K;
+	static int N, M;
+	static int[] dx = new int[]{-1, 1, 0, 0}, dy = new int[]{0, 0, -1, 1};
 	static boolean[][] board;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
+		while (T-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			M = Integer.parseInt(st.nextToken());
 			N = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
 			board = new boolean[N][M];
 			while (K-- > 0) {
 				st = new StringTokenizer(br.readLine());
-				int X = Integer.parseInt(st.nextToken());
-				int Y = Integer.parseInt(st.nextToken());
-				board[Y][X] = true;
+				int y = Integer.parseInt(st.nextToken());
+				int x = Integer.parseInt(st.nextToken());
+				board[x][y] = true;
 			}
 			int res = 0;
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < M; j++) {
 					if (board[i][j]) {
+						board[i][j] = false;
+						dfs(i, j);
 						res++;
-						bfs(i, j);
 					}
 				}
 			}
@@ -41,20 +35,13 @@ public class Main {
 		}
 	}
 
-	public static void bfs(int x, int y) {
-		int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
-		Deque<Place> q = new LinkedList<>();
-		q.add(new Place(x, y));
-		board[x][y] = false;
-		while (!q.isEmpty()) {
-			Place now = q.poll();
-			for (int i = 0; i < 4; i++) {
-				int nx = now.x + dx[i];
-				int ny = now.y + dy[i];
-				if (0 <= nx && nx < N && 0 <= ny && ny < M && board[nx][ny]) {
-					board[nx][ny] = false;
-					q.add(new Place(nx, ny));
-				}
+	public static void dfs(int x, int y) {
+		for (int d = 0; d < 4; d++) {
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			if (0 <= nx && nx < N && 0 <= ny && ny < M && board[nx][ny]) {
+				board[nx][ny] = false;
+				dfs(nx, ny);
 			}
 		}
 	}
