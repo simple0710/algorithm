cae19a4f070aa44adf8b9292914d7a44524dfeff
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-08 22:47:52 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 396 ms, Memory: 58048 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,29 +1,28 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N;
-	static int[] dx = new int[]{-1, -1, -1, 0, 0, 1, 1, 1}, dy = new int[]{-1, 0, 1, -1, 1, -1, 0, 1}; 
 	static String[][] board;
+	static int[] dx = new int[] {-1, -1, -1, 0, 1, 1, 1, 0}, dy = new int[] {-1, 0, 1, 1, 1, 0, -1, -1};
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
 		board = new String[N][N];
 		recur(N/3, N/2, N/2);
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				sb.append(board[i][j] == null ? " " : "*");
-			}
+		for (String[] row : board) {
+			for (String v : row) sb.append(v == null ? " " : "*");
 			sb.append("\n");
 		}
 		System.out.print(sb);
 	}
-	
-	public static void recur(int nowN, int centerX, int centerY) {
+
+	public static void recur(int nowN, int nowX, int nowY) {
 		if (nowN == 1) {
-			for (int i = 0 ; i < 8; i++) board[centerX + dx[i]][centerY + dy[i]] = "*";
+			for (int i = 0; i < 8; i++) board[nowX+dx[i]][nowY+dy[i]] = "*"; 
 			return;
 		}
-		for (int i = 0 ; i < 8; i++) recur(nowN/3, centerX + nowN * dx[i], centerY + nowN * dy[i]);
+		for (int i = 0; i < 8; i++) recur(nowN / 3, nowX + nowN * dx[i], nowY + nowN * dy[i]);
 	}
 }
