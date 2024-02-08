66d90abb0f1c8643c285ac1825e2df70b350481c
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-08 20:53:13 +0900

    [Gold II] Title: 네트워크 복구, Time: 724 ms, Memory: 92380 KB -BaekjoonHub

--- 백준/Gold/2211. 네트워크 복구/네트워크 복구.java
+++ 백준/Gold/2211. 네트워크 복구/네트워크 복구.java
@@ -4,28 +4,24 @@ import java.util.*;
 public class Main {
 	static final int MAX = Integer.MAX_VALUE;
 	static class Info {
-		int node;
-		Integer totalCost;
-		Integer nowCost;
-		Info(int node, int totalCost) {
-			this.node = node;
-			this.totalCost = totalCost;
+		int start;
+		int end;
+		Integer cost;
+		Info(int start, int end, int cost) {
+			this.start = start;
+			this.end = end;
+			this.cost = cost;
 		}
-		Info(int node, int totalCost, int nowCost) {
-			this.node = node;
-			this.totalCost = totalCost;
-			this.nowCost = nowCost;
-		}
-		
 	}
-	static int N, M;
-	static int[] defaultDistance;
+	static ArrayList<Info> answerList;
 	static ArrayList<Info>[] graph;
+	static int[] distance;
+	static boolean[] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		graph = new ArrayList[N+1];
 		for (int i = 0; i <= N; i++) graph[i] = new ArrayList<>();
 		while (M-- > 0) {
@@ -33,61 +29,42 @@ public class Main {
 			 int A = Integer.parseInt(st.nextToken());
 			 int B = Integer.parseInt(st.nextToken());
 			 int C = Integer.parseInt(st.nextToken());
-			 graph[A].add(new Info(B, C));
-			 graph[B].add(new Info(A, C));
+			 graph[A].add(new Info(A, B, C));
+			 graph[B].add(new Info(B, A, C));
 		}
-		defaultDistance = new int[N+1];
-		Arrays.fill(defaultDistance, MAX);
-		defaultDijkstra();
-		System.out.print(accessDijkstra());
+		answerList = new ArrayList<>();
+		visited = new boolean[N+1];
+		visited[1] = true;
+		distance = new int[N+1];
+		Arrays.fill(distance, MAX);
+		 dijkstra();
+		 System.out.println(answerList.size());
+		 for (Info now : answerList) {
+			 System.out.println(now.start + " " + now.end);
+		 }
 	}
 	
-	public static void defaultDijkstra() {
+	public static void dijkstra() {
 		PriorityQueue<Info> pq = new PriorityQueue<>(new Comparator<Info>() {
 			@Override
 			public int compare(Info o1, Info o2) {
-				return o1.totalCost.compareTo(o2.totalCost);
+				return o1.cost.compareTo(o2.cost);
 			}
 		});
-		pq.add(new Info(1, 0));
+		pq.add(new Info(1, 1, 0));
 		while (!pq.isEmpty()) {
 			Info now = pq.poll();
-			for (Info next : graph[now.node]) {
-				int nextCost = now.totalCost + next.totalCost;
-				if (nextCost < defaultDistance[next.node]) {
-					pq.add(new Info(next.node, nextCost));
-					defaultDistance[next.node] = nextCost;
+			if (!visited[now.end]) {
+				visited[now.end] = true;
+				answerList.add(now);
+			}
+			for (Info next : graph[now.end]) {
+				int nextCost = now.cost + next.cost;
+				if (nextCost < distance[next.end]) {
+					pq.add(new Info(next.start, next.end, nextCost));
+					distance[next.end] = nextCost;
 				}
 			}
 		}
-	}
-	
-	public static StringBuilder accessDijkstra() {
-		StringBuilder sb = new StringBuilder();
-		PriorityQueue<Info> pq = new PriorityQueue<>(new Comparator<Info>() {
-			@Override
-			public int compare(Info o1, Info o2) {
-				if (o1.nowCost == o2.nowCost) return o1.totalCost.compareTo(o2.totalCost);
-				return o1.nowCost.compareTo(o2.nowCost);
-			}
-		});
-		int cnt = 0;
-		int[] accessCheck = new int[N+1];
-		accessCheck[1] = 1;
-		pq.add(new Info(1, 0, 0));
-		while(!pq.isEmpty()) {
-			Info now = pq.poll();
-			for (Info next : graph[now.node]) {
-				int nextCost = now.totalCost + next.totalCost;
-				if (nextCost <= defaultDistance[next.node] && accessCheck[next.node] == 0) {
-					cnt++;
-					sb.append(now.node).append(" ").append(next.node).append("\n");
-					accessCheck[next.node] = now.node;
-					pq.add(new Info(next.node, nextCost, next.totalCost));
-				}
-			}
-		}
-		System.out.println(cnt);
-		return sb;
 	}
 }
