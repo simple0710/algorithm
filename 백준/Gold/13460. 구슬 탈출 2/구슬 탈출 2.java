bbe44e4c56f9cffc8d38412e7a384c3b98325c9b
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-11 21:25:17 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -19,7 +19,6 @@ public class Main {
 
 	static int N, M, rx, ry, bx, by;
 	static int[] dx = new int[] { -1, 0, 1, 0 }, dy = new int[] { 0, -1, 0, 1 };
-	static int[][][] visited;
 	static char[][] board;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -41,43 +40,35 @@ public class Main {
 				}
 			}
 		}
-		System.out.print(getRoot());
+		System.out.print(Solution());
 	}
 
-	public static int getRoot() {
+	public static int Solution() {
 		ArrayDeque<Place> q = new ArrayDeque<>();
-		q.add(new Place(0, rx, ry, bx, by));
+		boolean[][][][] visited = new boolean[N][M][N][M];
+		q.add(new Place(0, rx, ry, bx,by));
+		visited[rx][ry][bx][by] = true;
 		while (!q.isEmpty()) {
 			Place now = q.poll();
 			if (now.cnt >= 10) break;
 			for (int i = 0; i < 4; i++) {
 				int[] blueP;
 				int[] redP;
-				if (now.rx == now.bx || now.ry == now.by) {
-					if (now.rx < now.bx || now.ry < now.by) {
-						if (i < 2) {
-							redP = Move(now.rx, now.ry, now.bx, now.by, i);
-							blueP = Move(now.bx, now.by, redP[0], redP[1], i);
-						} else {
-							blueP = Move(now.bx, now.by, now.rx, now.ry, i);
-							redP = Move(now.rx, now.ry, blueP[0], blueP[1], i);
-						}
+				redP = Move(now.rx, now.ry, i);
+				blueP = Move(now.bx, now.by, i);
+				if (board[blueP[0]][blueP[1]] == 'O') continue;
+				if (board[redP[0]][redP[1]] == 'O') return now.cnt + 1;
+				if (redP[0] == blueP[0] && redP[1] == blueP[1]) {
+					if (redP[2] < blueP[2]) {
+						blueP[0] -= dx[i];
+						blueP[1] -= dy[i];
 					} else {
-						if (i < 2) {
-							blueP = Move(now.bx, now.by, now.rx, now.ry, i);
-							redP = Move(now.rx, now.ry, blueP[0], blueP[1], i);
-						} else {
-							redP = Move(now.rx, now.ry, now.bx, now.by, i);
-							blueP = Move(now.bx, now.by, redP[0], redP[1], i);
-						}
+						redP[0] -= dx[i];
+						redP[1] -= dy[i];
 					}
 				}
-				else {
-					redP = Move(now.rx, now.ry, now.bx, now.by, i);
-					blueP = Move(now.bx, now.by, now.rx, now.ry, i);
-				}
-				if (blueP[2] == 0) {
-					if (redP[2] == 1) return now.cnt + 1;
+				if (!visited[redP[0]][redP[1]][blueP[0]][blueP[1]]) {
+					visited[redP[0]][redP[1]][blueP[0]][blueP[1]] = true;
 					q.add(new Place(now.cnt + 1, redP[0], redP[1], blueP[0], blueP[1]));
 				}
 			}
@@ -85,16 +76,13 @@ public class Main {
 		return -1;
 	}
 
-	public static int[] Move(int x, int y, int ax, int ay, int d) {
-		int nx = x + dx[d];
-		int ny = y + dy[d];
-		int flag = 0;
-		while (board[nx][ny] != '#') {
-			if (board[nx][ny] == 'O') flag = 1;
-			if (nx == ax && ny == ay) break;
-			nx += dx[d];
-			ny += dy[d];
+	public static int[] Move(int x, int y, int d) {
+		int cnt = 0;
+		while (board[x + dx[d]][y + dy[d]] != '#' && board[x][y] != 'O') {
+			x += dx[d];
+			y += dy[d];
+			cnt++;
 		}
-		return new int[] { nx - dx[d], ny - dy[d], flag };
+		return new int[] { x, y, cnt };
 	}
 }
