cf8c4da0e5fb94d8d18a2e7cd9434049543f24cd
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-12 22:43:23 +0900

    [Gold IV] Title: 뱀, Time: 128 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -21,7 +21,6 @@ public class Main {
 	}
 	
 	static int N;
-	
 	static int[] dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
 	static boolean[][] snakeBoard;
 	public static void main(String[] args) throws IOException {
