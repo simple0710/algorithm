c648558d7bbd50f56fdf7c2bcdccc5d3af1753d8
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-15 22:22:56 +0900

    [Silver III] Title: N과 M (6), Time: 208 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Silver III/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] numsArr, resArr;
+	static boolean[] numsPick;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();;
+		numsArr = new int[N];
+		for (int i = 0; i < N; i++) numsArr[i] = sc.nextInt();
+		Arrays.sort(numsArr);
+		resArr = new int[M];
+		numsPick = new boolean[N];
+		back(0, 0);
+		System.out.print(sb);
+	}
+	
+	public static void back(int depth, int start) {
+		if (depth == M) {
+			for (int v : resArr) sb.append(v).append(" ");
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i < N; i++) {
+			if (!numsPick[i]) {
+				numsPick[i] = true;
+				resArr[depth] = numsArr[i]; 
+				back(depth + 1, i + 1);
+				numsPick[i] = false;
+			}
+		}
+	}
+}
