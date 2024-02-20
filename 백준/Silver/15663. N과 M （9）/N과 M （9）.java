ad85aee7bbfd4a4cb63f6afc134c7fd875905b5b
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-21 00:17:26 +0900

    [Silver II] Title: N과 M (9), Time: 276 ms, Memory: 28524 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] nums, numsPick, duplicatedPick;
+	static boolean[] pick;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		nums = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(nums);
+		pick = new boolean[N];
+		numsPick = new int[M];
+		duplicatedPick = new int[M+1];
+		back(0);
+		System.out.print(sb);
+	}
+
+	public static void back(int depth) {
+		if (depth == M) {
+			for (int v : numsPick) sb.append(v + " ");
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			if (pick[i] || numsPick[depth] == nums[i]) continue;
+			pick[i] = true;
+			numsPick[depth] = nums[i];
+			back(depth+1);
+			pick[i] = false;
+		}
+		numsPick[depth] = 0;
+	}
+}
