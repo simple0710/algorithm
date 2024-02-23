d67c5e504a098128b46d4e8af98f89d44d1b2cce
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-23 21:45:46 +0900

    [Gold IV] Title: ⚾, Time: 616 ms, Memory: 111252 KB -BaekjoonHub

---
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, res;
+	static int[] pick, playGround;
+	static int[][] scoreBoard;
+	static boolean[] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		scoreBoard = new int[N][9];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 9; j++) {
+				scoreBoard[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		pick = new int[9];
+		pick[3] = 0;
+		visited = new boolean[9];
+		visited[0] = true;
+		playGround = new int[8];
+		back(0);
+		System.out.print(res);
+	}
+	
+	public static void back(int depth) {
+		if (depth == 9) {
+			res = Math.max(res, play());
+			return;
+		}
+		if (depth == 3) back(depth+1);
+		else {
+			for (int i = 0; i < 9; i++) {
+				if (!visited[i]) {
+					visited[i] = true;
+					pick[depth] = i;
+					back(depth+1);
+					visited[i] = false;
+				}
+			}
+		}
+	}
+	
+	public static int play() {
+		int totalScore = 0;
+		int playCnt = 0;
+		int out = 0;
+		int now = 0;
+		while (playCnt < N) {
+			int nowRes = scoreBoard[playCnt][pick[now++]];
+			if (nowRes != 0) run(nowRes);
+			else out++;
+			now%=9;
+			if (out == 3) {
+				out = 0;
+				playCnt++;
+				for (int i = 4; i < 8; i++) {
+					totalScore += playGround[i];
+				}
+				playGround = new int[8];
+			}
+		}
+		return totalScore;
+	}
+	
+	public static void run(int num) {
+		for (int i = 3; i > 0; i--) {
+			if (0 < playGround[i]) {
+				playGround[i] = 0;
+				playGround[i+num] += 1;
+			}
+		}
+		playGround[num] += 1;
+	}
+}
