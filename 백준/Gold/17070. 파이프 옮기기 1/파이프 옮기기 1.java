0640891fed1ef993c35482253c5b059f6d9da98c
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-25 00:31:31 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,41 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, res;
-	static int[][] board;
-	static int[] dx = {1, 0, 1}, dy = {0, 1, 1};
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
-		for (int i = 0; i < N; i++) {
+		int N = Integer.parseInt(br.readLine());
+		int[][] board = new int[N+1][N+1];
+		for (int i = 1; i <= N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
+			for (int j = 1; j <= N; j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		res = 0;
-		dfs(0, 1, 0);
-		System.out.println(res);
-	}
-	
-	public static void dfs(int x, int y, int d) {
-		if (x < 0 || x >= N || y < 0 || y >= N || board[x][y] == 1) return;
-		if (x == N-1 && y == N-1) {
-			res++;
-			return;
+		int[][][] ch = new int[3][N+1][N+1];
+		ch[0][1][2] = 1;
+		for (int i = 1; i <= N; i++) {
+			for (int j = 3; j <= N; j++) {
+				if (board[i][j] == 0) {
+					ch[0][i][j] = ch[0][i][j-1] + ch[2][i][j-1];
+					ch[1][i][j] = ch[1][i-1][j] + ch[2][i-1][j];
+					if (board[i][j-1] == 0 && board[i-1][j] == 0) {
+						ch[2][i][j] = ch[0][i-1][j-1] + ch[1][i-1][j-1] + ch[2][i-1][j-1];
+					}
+				}
+			}
 		}
-		if (x+1 < N && y+1 < N && board[x+1][y] != 1 && board[x][y+1] != 1) {
-			dfs(x+1, y+1, 2);
-		}
-		
-		if (d == 0 || d == 2) dfs(x, y+1, 0);
-		if (d == 1 || d == 2) dfs(x+1, y, 1);
+		System.out.print(ch[0][N][N] + ch[1][N][N] + ch[2][N][N]);
 	}
 }
