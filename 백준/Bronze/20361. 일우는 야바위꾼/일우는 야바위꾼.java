7c55549709221f2899309f86448254603afe83af
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-27 19:12:00 +0900

    [Bronze III] Title: 일우는 야바위꾼, Time: 364 ms, Memory: 38000 KB -BaekjoonHub

---
+++ 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		while (K-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			if (A == X) X = B;
+			else if (B == X) X = A;
+		}
+		System.out.print(X);
+	}
+}
