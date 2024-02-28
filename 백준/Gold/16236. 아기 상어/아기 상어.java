ff63f6a7f47e370e8eb917d0ce32598c84441080
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-28 21:09:18 +0900

    [Gold III] Title: 아기 상어, Time: 136 ms, Memory: 15084 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -51,14 +51,8 @@ public class Main {
 				}
 			}
 		}
-		if (fishList.size() > 0) {
-			Collections.sort(fishList, new Comparator<int[]>() {
-				@Override
-				public int compare(int[] o1, int[] o2) {
-					if (o1[0] == o2[0]) return Integer.compare(o1[1], o2[1]);
-					return Integer.compare(o1[0], o2[0]);
-				}
-			});
+		if (!fishList.isEmpty()) {
+			Collections.sort(fishList, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
 			int[] fish = fishList.get(0);
 			board[fish[0]][fish[1]] = 0;
 			startX = fish[0];
@@ -68,7 +62,6 @@ public class Main {
 				eat = 0;
 			}
 			return visited[fish[0]][fish[1]] - 1;
-			
 		}
 		return 0;
 	}
