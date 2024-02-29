d64089c221a50aa0062e80cc27c5b73d76484aef
simple710 <112814424+simple0710@users.noreply.github.com>
2024-02-29 21:20:26 +0900

    [Platinum V] Title: 백조의 호수, Time: 760 ms, Memory: 162512 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.java
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.java
@@ -10,62 +10,56 @@ public class Main {
 			this.y = y;
 		}
 	}
-	static int N, M;
+	static int R, C;
 	static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
-	static boolean flag;
-	static String[][] board;
-	static Place end;
+	static char[][] board;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		board = new String[N][M];
-		ArrayDeque<Place> bq = new ArrayDeque<>(), q = new ArrayDeque<>();
-		boolean[][] visited1 = new boolean[N][M];
-		boolean[][] visited2 = new boolean[N][M];
-		for (int i = 0; i < N; i++) {
-			board[i] = br.readLine().split("");
-			for (int j = 0; j < M; j++) {
-				if (board[i][j].equals("L")) {
-					board[i][j] = ".";
-					if(bq.isEmpty()) {
-						visited1[i][j] = true;
-						bq.add(new Place(i, j));
-					} else end = new Place(i, j);
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		boolean[][] visited1 = new boolean[R][C];
+		boolean[][] visited2 = new boolean[R][C];
+		board = new char[R][C];
+		ArrayDeque<Place> bird = new ArrayDeque<>();
+		ArrayDeque<Place> water = new ArrayDeque<>();
+		Place end = null;
+		for (int i = 0; i < R; i++) {
+			board[i] = br.readLine().toCharArray();
+			for (int j = 0; j < C; j++) {
+				if (board[i][j] == 'L') {
+					board[i][j] = '.';
+					if (bird.isEmpty()) bird.add(new Place(i, j));
+					else end = new Place(i, j);
 				}
-				if (board[i][j].equals(".")) {
-					visited2[i][j] = true;
-					q.add(new Place(i, j));
+				if (board[i][j] == '.') {
+					water.add(new Place(i, j));
+					visited1[i][j] = true;
 				}
 			}
 		}
-		int t = 0;
+		int t = 1;
 		while (true) {
-			q = bfs(q, visited2, true);
-			bq = bfs(bq, visited1, false);
-			if (visited1[end.x][end.y]) break;
+			water = bfs(water, visited1, true);
+			bird = bfs(bird, visited2, false);
+			if (visited2[end.x][end.y]) break;
 			t++;
 		}
 		System.out.print(t);
 	}
-	
 	public static ArrayDeque<Place> bfs(ArrayDeque<Place> q, boolean[][] visited, boolean flag) {
 		ArrayDeque<Place> newQ = new ArrayDeque<>();
 		while (!q.isEmpty()) {
 			Place now = q.poll();
-			if (flag) board[now.x][now.y] = ".";
-			else if (end.equals(now)) {
-				newQ.clear();
-				break;
-			}
 			for (int i = 0; i < 4; i++) {
 				int nx = now.x + dx[i];
 				int ny = now.y + dy[i];
-				if (0 <= nx && nx < N && 0 <= ny && ny < M && !visited[nx][ny]) {
+				if (0 <= nx && nx < R && 0 <= ny && ny < C && !visited[nx][ny]) {
 					visited[nx][ny] = true;
-					if (board[nx][ny].equals("X")) newQ.add(new Place(nx, ny));
-					else q.add(new Place(nx, ny));
+					if (board[nx][ny] == 'X') {
+						newQ.add(new Place(nx, ny));
+						if (flag) board[nx][ny] = '.';
+					} else q.add(new Place(nx, ny));
 				}
 			}
 		}
