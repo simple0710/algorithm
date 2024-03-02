36739fd8d413d117b9be4c474cb21193ae21522a
simple710 <112814424+simple0710@users.noreply.github.com>
2024-03-02 23:29:30 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 564 ms, Memory: 92292 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, res;
+	static int[] sortArr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		res = 1;
+		sortArr = new int[N+1];
+		Arrays.fill(sortArr, Integer.MAX_VALUE);
+		for (int i = 1; i < N+1; i++) {
+			binarySearch(Integer.parseInt(st.nextToken()));
+		}
+		System.out.print(res);
+	}
+
+	public static void binarySearch(int find) {
+		int s = 0, e = N-1;
+		while (s < e) {
+			int mid = (s + e) / 2;
+			if (find <= sortArr[mid]) e = mid;
+			else s = mid + 1;
+		}
+		res = Math.max(res, 1 + e);
+		sortArr[e] = find;
+	}
+}
