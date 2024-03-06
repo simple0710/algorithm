3129b27d16f8cc7785996ba35ed09cea3472ff63
simple710 <112814424+simple0710@users.noreply.github.com>
2024-03-06 21:34:51 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 608 ms, Memory: 42860 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		while (T-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,8 +21,9 @@ public class Main {
 					s++;
 				}
 			}
-			System.out.println(res);
+			sb.append(res).append("\n");
 		}
+		System.out.print(sb);
 	}
 	
 	static ArrayList<Integer> getSortList(StringTokenizer st) {
