2d456a34e141e20d267736db425e2145a429254d
simple710 <112814424+simple0710@users.noreply.github.com>
2024-04-22 20:51:47 +0900

    [Bronze IV] Title: 특별한 가지, Time: 120 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        System.out.print(M / N * K);
+    }
+}
