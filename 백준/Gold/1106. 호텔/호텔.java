aea2edfaa1c0f7707f9af59b9a948fc9f9376e2e
simple710 <112814424+simple0710@users.noreply.github.com>
2024-04-28 15:04:16 +0900

    [Gold IV] Title: 호텔, Time: 124 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.java
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -1,24 +1,23 @@
 import java.io.*;
 
 public class Main {
-	static int MAX = 100_001;
+	static int MAX;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] str = br.readLine().split(" ");
 		int C = Integer.parseInt(str[0]);
 		int N = Integer.parseInt(str[1]);
+		MAX = C+101;
 		int[] dp = new int[MAX];
+		for (int i = 1; i < MAX; i++) dp[i] = 1000 * MAX;
 		while (N-- > 0) {
 			str = br.readLine().split(" ");
 			int cost = Integer.parseInt(str[0]);
 			int guest = Integer.parseInt(str[1]);
-			for (int i = cost; i < MAX; i++) dp[i] = Math.max(dp[i], dp[i-cost] + guest);
+			for (int i = guest; i < MAX; i++) dp[i] = Math.min(dp[i], dp[i-guest] + cost);
 		}
-		for (int i = 0; i < MAX; i++) {
-			if (dp[i] >= C) {
-				System.out.print(i);
-				break;
-			}
-		}
+		int res = 1000*MAX;
+		for (int i = C; i < MAX; i++) res = Math.min(res, dp[i]);
+		System.out.print(res);
 	}
 }
