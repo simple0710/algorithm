3c842ad992d9765fc4b0e8dd966c48c17f126937
simple710 <112814424+simple0710@users.noreply.github.com>
2024-05-01 13:48:12 +0900

    [Gold IV] Title: 카우버거 알바생, Time: 168 ms, Memory: 14840 KB -BaekjoonHub

---
+++ 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int M = Integer.parseInt(input[1]);
+		int K = Integer.parseInt(input[2]);
+		int[][] dp = new int[M+1][K+1];
+		while (N-- > 0) {
+			input = br.readLine().split(" ");
+			int x = Integer.parseInt(input[0]);
+			int y = Integer.parseInt(input[1]);
+			for (int i = M; i >= x; i--) {
+				for (int j = K; j >= y; j--) {
+					dp[i][j] = Math.max(dp[i][j], dp[i-x][j-y] + 1);
+				}
+			}
+		}
+		System.out.print(dp[M][K]);
+	}
+}
