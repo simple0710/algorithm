704c11d33406b3fbe357aa9e92e851565f59bc92
simple710 <112814424+simple0710@users.noreply.github.com>
2024-05-05 13:46:41 +0900

    [Gold III] Title: 시로코와 은행털기, Time: 432 ms, Memory: 16872 KB -BaekjoonHub

---
+++ 백준/Gold/26607. 시로코와 은행털기/시로코와 은행털기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int K = Integer.parseInt(input[1]);
+		int X = Integer.parseInt(input[2]);
+		int MAX = K * X;
+		boolean[][] dp = new boolean[K + 1][MAX + 1];
+		dp[0][0] = true;
+		while (N-- > 0) {
+			input = br.readLine().split(" ");
+			int a = Integer.parseInt(input[0]);
+			int b = Integer.parseInt(input[1]);
+			for (int i = K; i >= 1; i--) {
+				for (int j = a; j <= MAX; j++) {
+					dp[i][j] = dp[i][j] || dp[i - 1][j - a];
+				}
+			}
+		}
+		int res = 0;
+		for (int i = 1; i <= MAX; i++) {
+			if (dp[K][i])
+				res = Math.max(res, i * (MAX - i));
+		}
+		System.out.print(res);
+	}
+}
