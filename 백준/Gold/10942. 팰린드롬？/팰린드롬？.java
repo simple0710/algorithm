7ef533204804a214ae594eee9d51645d42339273
simple710 <112814424+simple0710@users.noreply.github.com>
2024-06-22 00:36:49 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1544 ms, Memory: 305860 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N+1];
+        String[] input = br.readLine().split(" ");
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(input[i-1]);
+        }
+        int[][] check = new int[N+1][N+1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = i; j <= N; j++) {
+                if (checkF(arr, i, j)) check[i][j] = 1;
+            }
+        }
+        int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (M-- > 0) {
+            input = br.readLine().split(" ");
+            int s = Integer.parseInt(input[0]);
+            int e = Integer.parseInt(input[1]);
+            sb.append(check[s][e]).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static boolean checkF(int[] arr, int s, int e) {
+        while (s < e) {
+            if (arr[s++] != arr[e--]) return false;
+        }
+        return true;
+    }
+}
