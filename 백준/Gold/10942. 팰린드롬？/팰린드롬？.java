8ef716c431555ad2a1d404eb5fcb982845e969e7
simple710 <112814424+simple0710@users.noreply.github.com>
2024-06-22 20:43:23 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1076 ms, Memory: 305672 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -6,13 +6,12 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N+1];
         String[] input = br.readLine().split(" ");
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(input[i-1]);
-        }
-        int[][] check = new int[N+1][N+1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = i; j <= N; j++) {
-                if (checkF(arr, i, j)) check[i][j] = 1;
+        for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(input[i-1]);
+        int[][] dp = new int[N+1][N+1];
+        for (int i = 0; i < N; i++) {
+            for (int s = 1; s < N+1-i; s++) {
+                int e = s + i;
+                if (s == e || (arr[s] == arr[e] && (dp[s+1][e-1] == 1 || s + 1 == e))) dp[s][e] = 1;
             }
         }
         int M = Integer.parseInt(br.readLine());
@@ -21,15 +20,8 @@ public class Main {
             input = br.readLine().split(" ");
             int s = Integer.parseInt(input[0]);
             int e = Integer.parseInt(input[1]);
-            sb.append(check[s][e]).append("\n");
+            sb.append(dp[s][e]).append("\n");
         }
         System.out.print(sb);
-    }
-
-    private static boolean checkF(int[] arr, int s, int e) {
-        while (s < e) {
-            if (arr[s++] != arr[e--]) return false;
-        }
-        return true;
     }
 }
