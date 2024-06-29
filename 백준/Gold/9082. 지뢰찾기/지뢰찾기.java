985e0eeee8616dff427ec7e560bef15ebaf88ee3
simple710 <112814424+simple0710@users.noreply.github.com>
2024-06-29 11:19:26 +0900

    [Gold IV] Title: 지뢰찾기, Time: 120 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Gold/9082. 지뢰찾기/지뢰찾기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+public class Main {
+    static int[] dx = {-1, 0, 1};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            int res = 0;
+            String[] nums = br.readLine().split("");
+            int[] numArr = new int[N];
+            for (int i = 0; i < N; i++) numArr[i] = Integer.parseInt(nums[i]);
+
+            String[] words = br.readLine().split("");
+            for (int i = 0; i < N; i++) {
+                if (words[i].equals("*")) {
+                    res++;
+                    for (int d : dx) {
+                        int ni = i + d;
+                        if (0 <= ni && ni < N) numArr[ni]--;
+                    }
+                }
+            }
+
+            for (int i = 0; i < N-1; i++){
+                if (!words[i].equals("*")) {
+                    boolean flag = true;
+                    for (int d : dx) {
+                        int ni = i + d;
+                        if (0 <= ni && ni < N && numArr[ni] == 0) flag = false;
+                    }
+                    if (flag) {
+                        res++;
+                        for (int d: dx) {
+                            int ni = i + d;
+                            if (0 <= ni && ni < N) numArr[ni]--;
+                        }
+                    }
+                }
+            }
+            if (numArr[N-1] == 1) res++;
+            System.out.println(res);
+        }
+    }
+}
