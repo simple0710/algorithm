36767cc352638ffd96c32da41da22eaca8bc12fe
simple710 <112814424+simple0710@users.noreply.github.com>
2024-08-10 23:54:36 +0900

    [Bronze V] Title: CAPS, Time: 276 ms, Memory: 21040 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String string = br.readLine().toUpperCase();
+        System.out.print(string);
+    }
+}
