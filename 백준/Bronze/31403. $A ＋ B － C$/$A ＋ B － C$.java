23cbdf788432cafe2cb1b52099470fc6335f6597
simple710 <112814424+simple0710@users.noreply.github.com>
2024-09-13 20:29:07 +0900

    [Bronze IV] Title: $A + B - C$, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        String c = br.readLine();
+        System.out.println(Integer.parseInt(a) + Integer.parseInt(b) - Integer.parseInt(c));
+        System.out.print(Integer.parseInt(a + b) - Integer.parseInt(c));
+    }
+}
