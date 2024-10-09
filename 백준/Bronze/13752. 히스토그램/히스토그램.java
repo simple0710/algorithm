7c96d75ff8aa887bab7ba7583b53c67ee3896fd0
simple710 <112814424+simple0710@users.noreply.github.com>
2024-10-10 02:59:05 +0900

    [Bronze IV] Title: 히스토그램, Time: 180 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int testCase = sc.nextInt();
+        
+        for (int i = 0; i < testCase; i++) {
+            int histogram_val = sc.nextInt();
+            String unit_bar = "=";
+            String histogram_bar = unit_bar.repeat(histogram_val);
+            
+            System.out.println(histogram_bar);
+        }
+    }
+}
